0ce1b1b8693c3ad56bb7d8531094902679347819
Alex <tripscape+github@protonmail.com>
2026-05-30 15:11:53 -0500

    Update deploy target

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+set -e
 echo "Building site..."
 npm run build
-
 echo "Deploying to server..."
-rsync -avz --delete dist/ rg@104.237.128.61:/var/www/alexanderkey.com/_site
-
+rsync -avz --delete dist/ deploy@45.79.25.166:/var/www/alexanderkey.com/_site
 echo "Deployment complete!"
